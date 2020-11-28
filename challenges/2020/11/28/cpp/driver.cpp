77ab9fec4908d6020455af4aca782cf9bf4944d6
Sam Davis <sam@samgd.com>
2020-11-28 22:05:45 +0000

    Daily challenge 2020-11-28

---
+++ challenges/2020/11/28/cpp/driver.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+#include "solution.h"
+
+int main()
+{
+    // {window size, input, expected output}
+    std::vector<std::tuple<int, std::vector<int>, std::vector<int>>>  tests {
+        std::make_tuple<int, std::vector<int>, std::vector<int>>(
+            3, {1, 3, -1, -3, 5, 3, 6, 7}, {3, 3, 5, 5, 6, 7}
+        ),
+        std::make_tuple<int, std::vector<int>, std::vector<int>>(
+            1, {1}, {1}
+        )
+    };
+
+    Solution s;
+
+    for (auto &test : tests) {
+        auto result = s.maxSlidingWindow(std::get<1>(test), std::get<0>(test));
+
+        if (!std::equal(
+                result.begin(), result.end(),
+                std::get<2>(test).begin(), std::get<2>(test).end()))
+        {
+            std::cout << "Test failed!" << std::endl;
+            return 1;
+        }
+    }
+
+    std::cout << "Tests pass!" << std::endl;
+}
---
+++ challenges/2020/11/28/cpp/solution.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <algorithm>
+#include <set>
+#include <vector>
+
+
+class Window {
+public:
+    // Extracts `last_elem` from the Window and inserts `new_elem`. Returns the
+    // maximum value in the Window after the removal and insertion has
+    // completed.
+    inline int insert(int last_elem, int new_elem)
+    {
+        elements.extract(last_elem);
+        elements.insert(new_elem);
+        return max();
+    }
+
+    // Inserts `new_elem` into the Window.
+    inline void insert(int new_elem)
+    {
+        elements.insert(new_elem);
+    }
+
+    // Returns the maximum value in the Window.
+    inline int max() const {
+        return *elements.rbegin();
+    }
+private:
+    std::multiset<int> elements;
+};
+
+
+class Solution {
+public:
+    // maxSlidingWindow slides a window of size k over nums and returns a
+    // vector containing the maximum values in each window position.
+    //
+    // This implementation is O(nlogk) but likely has a higher constant than
+    // the naive O(nk) implementation (where `n = nums.size()`).
+    std::vector<int> maxSlidingWindow(std::vector<int> &nums, int k) {
+        std::vector<int> results;
+        // Problem constraints specify: 1 <= k <= nums.length
+        // This means we can assume results will contain this many elements.
+        results.reserve(static_cast<int>(nums.size()) - k + 1);
+
+        Window w;
+
+        // Prime window with k values before finding first maximum.
+        for (int i = 0; i < k; i++) {
+            w.insert(nums[i]);
+        }
+        results.push_back(w.max());
+
+        // Iterate over all remaining window positions (remove last, add new,
+        // push new maximum to results).
+        for (int i = k; i < static_cast<int>(nums.size()); i++) {
+            results.push_back(w.insert(nums[i - k], nums[i]));
+        }
+
+        return results;
+    }
+};
